ecc35c43786c9b346d56336eb2ae721a0edb9625
xb205 <62425964+devxb@users.noreply.github.com>
2024-01-24 17:16:32 +0900

    refactor: 서버통신 uri 환경변수로 분리 (#29)
    * refactor: 서버통신 uri를 환경변수로 분리한다
    
    * refactor: StreamReceiver를 재사용하도록 수정한다

--- src/main/kotlin/org/rooftop/shop/infra/WebClientConfigurer.kt
+++ src/main/kotlin/org/rooftop/shop/infra/WebClientConfigurer.kt
@@ -1,5 +1,6 @@
 package org.rooftop.shop.infra
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -7,8 +8,10 @@ import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 @Profile("prod")
-class WebClientConfigurer {
+class WebClientConfigurer(
+    @Value("\${rooftop.server.identity:http://identity.rooftopmsa.org}") private val identityServerUri: String,
+) {
 
     @Bean
-    fun identityWebClient(): WebClient = WebClient.create("http://identity.rooftopmsa.org")
+    fun identityWebClient(): WebClient = WebClient.create(identityServerUri)
 }
