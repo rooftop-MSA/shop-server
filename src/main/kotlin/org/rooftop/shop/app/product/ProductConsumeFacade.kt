21899de74a9d1a3571ab4f6b187df545186c7066
xb205 <62425964+devxb@users.noreply.github.com>
2024-01-24 12:30:47 +0900

    fix: consumeProductFacade에서 rollback과 commit이 발행되지 않는 버그 수정 (#27)

--- src/main/kotlin/org/rooftop/shop/app/product/ProductConsumeFacade.kt
+++ src/main/kotlin/org/rooftop/shop/app/product/ProductConsumeFacade.kt
@@ -4,6 +4,7 @@ import org.rooftop.api.shop.ProductConsumeReq
 import org.rooftop.shop.domain.product.ProductService
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 
 @Service
 class ProductConsumeFacade(
@@ -15,12 +16,8 @@ class ProductConsumeFacade(
         return transactionManager.exists(productConsumeReq.transactionId)
             .joinTransaction(productConsumeReq)
             .consumeProduct(productConsumeReq)
-            .doOnSuccess {
-                transactionManager.commit(productConsumeReq.transactionId.toString())
-            }.doOnError {
-                transactionManager.rollback(productConsumeReq.transactionId.toString())
-                throw it
-            }
+            .commitOnSuccess(productConsumeReq)
+            .rollbackOnError(productConsumeReq)
     }
 
     private fun Mono<String>.joinTransaction(productConsumeReq: ProductConsumeReq): Mono<String> {
@@ -35,6 +32,23 @@ class ProductConsumeFacade(
     private fun Mono<String>.consumeProduct(productConsumeReq: ProductConsumeReq): Mono<Unit> {
         return this.flatMap {
             productService.consumeProduct(productConsumeReq)
+        }
+    }
+
+    private fun <T> Mono<T>.commitOnSuccess(productConsumeReq: ProductConsumeReq): Mono<T> {
+        return this.doOnSuccess {
+            transactionManager.commit(productConsumeReq.transactionId.toString())
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe()
+        }
+    }
+
+    private fun <T> Mono<T>.rollbackOnError(productConsumeReq: ProductConsumeReq): Mono<T> {
+        return this.doOnError {
+            transactionManager.rollback(productConsumeReq.transactionId)
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe()
+            throw it
         }
     }
 }
