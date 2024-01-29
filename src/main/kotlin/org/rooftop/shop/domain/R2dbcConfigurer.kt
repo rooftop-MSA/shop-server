295f485f7808512916c80256795ba7d569fab8c2
devxb <dlwnsdud205@naver.com>
2024-01-29 17:31:00 +0900

    refactor: 사용하지 않는 R2dbcAuditing import 를 삭제한다

--- src/main/kotlin/org/rooftop/shop/domain/R2dbcConfigurer.kt
+++ src/main/kotlin/org/rooftop/shop/domain/R2dbcConfigurer.kt
@@ -3,7 +3,6 @@ package org.rooftop.shop.domain
 import org.rooftop.shop.Application
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @Configuration
