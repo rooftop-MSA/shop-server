d73c838ff3c767a874c1bbf63f93ae422f4b0ead
devxb <dlwnsdud205@naver.com>
2024-02-21 12:37:14 +0900

    feat: Retry 로직을 트랜잭션 밖으로 뺀다

--- src/main/kotlin/org/rooftop/shop/app/product/TransactionEventHandler.kt
+++ src/main/kotlin/org/rooftop/shop/app/product/TransactionEventHandler.kt
@@ -3,13 +3,15 @@ package org.rooftop.shop.app.product
 import org.rooftop.netx.api.TransactionRollbackEvent
 import org.rooftop.netx.api.TransactionRollbackHandler
 import org.rooftop.netx.meta.TransactionHandler
-import org.rooftop.shop.domain.product.ProductRollbackEvent
-import org.springframework.context.ApplicationEventPublisher
+import org.rooftop.shop.domain.product.ProductService
 import reactor.core.publisher.Mono
+import reactor.util.retry.RetrySpec
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.toJavaDuration
 
 @TransactionHandler
 class TransactionEventHandler(
-    private val applicationEventPublisher: ApplicationEventPublisher,
+    private val productService: ProductService,
 ) {
 
 
@@ -33,17 +35,23 @@ class TransactionEventHandler(
     private fun Mono<Map<String, String>>.dispatch(): Mono<Unit> {
         return this.doOnNext {
             when (it["type"]) {
-                "consumeProduct" -> applicationEventPublisher.publishEvent(
-                    ProductRollbackEvent(
-                        it["productId"]?.toLong()
-                            ?: throw IllegalStateException("Type \"consumeProduct\" must have \"productId\" field"),
-                        it["consumeQuantity"]?.toLong()
-                            ?: throw IllegalStateException("Type \"consumeProduct\" must have \"consumeQuantity\" field")
-                    )
-                )
+                "consumeProduct" -> productService.rollbackProduct(
+                    it["productId"]?.toLong()
+                        ?: throw IllegalStateException("Type \"consumeProduct\" must have \"productId\" field"),
+                    it["consumeQuantity"]?.toLong()
+                        ?: throw IllegalStateException("Type \"consumeProduct\" must have \"consumeQuantity\" field")
+                ).retryWhen(jitterSpec)
 
                 else -> error("Cannot find matched replay type \"${it["type"]}\"")
             }
         }.map { }
+    }
+
+    private companion object Retry {
+        private const val RETRY_MOST_100_PERCENT = 1.0
+
+        private val jitterSpec =
+            RetrySpec.fixedDelay(Long.MAX_VALUE, 50.milliseconds.toJavaDuration())
+                .jitter(RETRY_MOST_100_PERCENT)
     }
 }
--- src/main/kotlin/org/rooftop/shop/domain/product/ProductRollbackEvent.kt
+++
@@ -1,6 +0,0 @@
-package org.rooftop.shop.domain.product
-
-data class ProductRollbackEvent(
-    val productId: Long,
-    val consumeQuantity: Long,
-)
